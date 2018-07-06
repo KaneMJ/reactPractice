9e45fe4e5ba5a4548cbaad209357550df4403101
KaneMJ <matthewjkane@live.com>
2018-07-05 20:55:49 -0700

    Added React Router for page routing

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,41 @@
 import React from 'react';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <header className="top">
-                <h1>
-                    Catch
-                    <span className="ofThe">
-                        <span className="of">Of</span>
-                        <span className="the">The</span>
-                    </span>
-                    <span>Day</span></h1>
-                <h3 className="tagline">
-                    <span>{this.props.tagline}</span>
-                </h3>
-            </header>
-        );
-    }
-}
+//Stateless functional component in ES6
+const Header = (props) => (
+    <header className="top">
+        <h1>
+            Catch
+            <span className="ofThe">
+                <span className="of">Of</span>
+                <span className="the">The</span>
+            </span>
+            Day
+            </h1>
+        <h3 className="tagline">
+            <span>{props.tagline}</span>
+        </h3>
+    </header>
+);
+
+// Normal React Component
+// class Header extends React.Component {
+//     render() {
+//         return (
+//             <header className="top">
+//                 <h1>
+//                     Catch
+//                     <span className="ofThe">
+//                         <span className="of">Of</span>
+//                         <span className="the">The</span>
+//                     </span>
+//                     Day
+//                 </h1>
+//                 <h3 className="tagline">
+//                     <span>{this.props.tagline}</span>
+//                 </h3>
+//             </header>
+//         );
+//     }
+// }
 
 export default Header;
