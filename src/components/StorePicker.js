0ac018249b9b165245bc44b24a2a539fb0761b97
Matthew <matthewjkane@live.com>
2018-07-07 11:12:20 -0700

    Binding methods to ensure proper this

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
 class StorePicker extends React.Component {
+    constructor() {
+        super();
+        this.goToStore = this.goToStore.bind(this);
+        console.log('Gonna create a component');
+    }
+    
+    myInput = React.createRef();
+
+    goToStore(event) {
+        event.preventDefault();
+        console.log(this);
+    }
+
     render() {
         return (
             <React.Fragment>
             {/* comment here */}
-            <form className="store-selector">
+            <form className="store-selector" onSubmit={this.goToStore}>
                 <h2>Please Enter a Store</h2>
-                <input type="text" required placeholder="Store Name" />
+                <input ref={this.myInput} type="text" required placeholder="Store Name" />
                 <button type="submit">Visit Store -></button>
             </form>
             </React.Fragment>
